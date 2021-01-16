c0675922692a6c7dffbc38103b9d9a6e22ec98fd
Fabien <contact@fabien-hebuterne.fr>
2021-01-16 20:57:11 +0100

    feat : add latest spigot version in CI

---
+++ buildtools.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+  echo "error : missing spigot version parameter"
+  exit 1
+fi
+
+mkdir buildtools && cd ./buildtools || exit
+
+curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O BuildTools.jar
+
+java -jar BuildTools.jar --rev "$1" --output-dir ../tmp
+
+rm -rf buildtools
