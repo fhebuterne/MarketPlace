66153facd6bde7e7accb02b3c0749f4a1fb90a9c
Fabien <contact@fabien-hebuterne.fr>
2021-04-17 16:25:45 +0200

    chore : add missing unit test on mails command

--- common/src/test/kotlin/fr/fabienhebuterne/marketplace/commands/CommandMailsTest.kt
+++ common/src/test/kotlin/fr/fabienhebuterne/marketplace/commands/CommandMailsTest.kt
@@ -20,6 +20,7 @@ import org.kodein.di.singleton
 class CommandMailsTest : BaseTest() {
 
     private val commandMailPermission = "marketplace.mails"
+    private val commandMailOtherPermission = "marketplace.mails.other"
     private val command: Command = mockk()
     private val mailsServiceMock: MailsService = mockk()
     private val mailsInventoryService: MailsInventoryService = mockk()
@@ -78,7 +79,7 @@ class CommandMailsTest : BaseTest() {
         every { command.aliases } returns arrayListOf()
         every { marketPlace.isReload } returns false
         every { playerMock.hasPermission(commandMailPermission) } returns true
-        every { playerMock.hasPermission("marketplace.mails.other") } returns false
+        every { playerMock.hasPermission(commandMailOtherPermission) } returns false
         every { playerMock.sendMessage(translation.errors.missingPermission) } just Runs
 
         // WHEN
@@ -98,7 +99,7 @@ class CommandMailsTest : BaseTest() {
         // THEN
         verify(exactly = 1) {
             playerMock.hasPermission(commandMailPermission)
-            playerMock.hasPermission("marketplace.mails.other")
+            playerMock.hasPermission(commandMailOtherPermission)
             playerMock.sendMessage(translation.errors.missingPermission)
         }
     }
@@ -118,7 +119,7 @@ class CommandMailsTest : BaseTest() {
 
         every { command.aliases } returns arrayListOf()
         every { playerMock.hasPermission(commandMailPermission) } returns true
-        every { playerMock.hasPermission("marketplace.mails.other") } returns true
+        every { playerMock.hasPermission(commandMailOtherPermission) } returns true
         every { mailsServiceMock.findUuidByPseudo(playerName) } returns ergailUuid
         every { marketPlace.isReload } returns false
         every { mailsServiceMock.getPaginated(pagination = pagination) } returns pagination
@@ -142,11 +143,92 @@ class CommandMailsTest : BaseTest() {
         // THEN
         verify(exactly = 1) {
             playerMock.hasPermission(commandMailPermission)
-            playerMock.hasPermission("marketplace.mails.other")
+            playerMock.hasPermission(commandMailOtherPermission)
             mailsServiceMock.findUuidByPseudo(playerName)
             mailsServiceMock.getPaginated(pagination = pagination)
             mailsInventoryService.initInventory(pagination, playerMock)
             playerMock.openInventory(inventory)
+        }
+    }
+
+    @Test
+    fun `should open mails inventory as an admin with uuid in argument with mails command`() {
+        // GIVEN
+        val inventory: Inventory = mockk()
+        val inventoryView: InventoryView = mockk()
+        val offlinePlayer: OfflinePlayer = mockk()
+        every { offlinePlayer.uniqueId } returns ergailUuid
+        val pagination = Pagination<Mails>(
+            currentPlayer = offlinePlayer.uniqueId,
+            viewPlayer = playerMock.uniqueId
+        )
+
+        every { command.aliases } returns arrayListOf()
+        every { playerMock.hasPermission(commandMailPermission) } returns true
+        every { playerMock.hasPermission(commandMailOtherPermission) } returns true
+        every { marketPlace.isReload } returns false
+        every { mailsServiceMock.getPaginated(pagination = pagination) } returns pagination
+        every { mailsInventoryService.initInventory(pagination, playerMock) } returns inventory
+        every { playerMock.openInventory(inventory) } returns inventoryView
+
+        // WHEN
+        val callCommandFactoryInit = CallCommandFactoryInit(marketPlace, "marketplace")
+        callCommandFactoryInit.onCommand(
+            playerMock,
+            command,
+            commandLabel,
+            arrayOf("mails", ergailUuid.toString()),
+            MarketPlace::class.java.classLoader,
+            commandPath,
+            permissionPrefix,
+            true,
+            kodein
+        )
+
+        // THEN
+        verify(exactly = 1) {
+            playerMock.hasPermission(commandMailPermission)
+            playerMock.hasPermission(commandMailOtherPermission)
+            mailsServiceMock.getPaginated(pagination = pagination)
+            mailsInventoryService.initInventory(pagination, playerMock)
+            playerMock.openInventory(inventory)
+        }
+    }
+
+    @Test
+    fun `should cannot open mails inventory as an admin when uuid not exist with mails command`() {
+        // GIVEN
+        val playerName = "truc"
+
+        every { command.aliases } returns arrayListOf()
+        every { playerMock.hasPermission(commandMailPermission) } returns true
+        every { playerMock.hasPermission(commandMailOtherPermission) } returns true
+        every { marketPlace.isReload } returns false
+        every { mailsServiceMock.findUuidByPseudo(playerName) } returns null
+
+        // WHEN
+        val callCommandFactoryInit = CallCommandFactoryInit(marketPlace, "marketplace")
+        callCommandFactoryInit.onCommand(
+            playerMock,
+            command,
+            commandLabel,
+            arrayOf("mails", playerName),
+            MarketPlace::class.java.classLoader,
+            commandPath,
+            permissionPrefix,
+            true,
+            kodein
+        )
+
+        // THEN
+        verify(exactly = 1) {
+            playerMock.hasPermission(commandMailPermission)
+            playerMock.hasPermission(commandMailOtherPermission)
+            mailsServiceMock.findUuidByPseudo(playerName)
+        }
+
+        verify(exactly = 0) {
+            playerMock.openInventory(any<Inventory>())
         }
     }
 
