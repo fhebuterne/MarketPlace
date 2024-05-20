ff2e3d44364883dbf7dbd843146c605f45dc9bb3
Fabien <contact@fabien-hebuterne.fr>
2024-05-20 18:57:07 +0200

    fix #36 : fix build issue

--- common/src/main/kotlin/fr/fabienhebuterne/marketplace/utils/CommodoreService.kt
+++ common/src/main/kotlin/fr/fabienhebuterne/marketplace/utils/CommodoreService.kt
@@ -4,12 +4,12 @@ import com.mojang.brigadier.tree.LiteralCommandNode
 import me.lucko.commodore.Commodore
 import me.lucko.commodore.CommodoreProvider
 import me.lucko.commodore.file.CommodoreFileReader
-import org.bukkit.command.Command
+import org.bukkit.command.PluginCommand
 import org.bukkit.plugin.java.JavaPlugin
 
 class CommodoreService {
 
-    fun init(plugin: JavaPlugin, command: Command) {
+    fun init(plugin: JavaPlugin, command: PluginCommand?) {
         val commodore: Commodore = CommodoreProvider.getCommodore(plugin)
         val marketPlaceCommand: LiteralCommandNode<Any> =
             CommodoreFileReader.INSTANCE.parse(plugin.getResource("marketplace.commodore"))
